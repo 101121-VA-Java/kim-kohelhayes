03899c1ee9e84d3f721f6ff824571f538a8451a3
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-10-27 09:05:44 -0500

    making sure I'm up to date

--- Proj-Zero/src/main/java/com/kim/repositories/ItemArray.java
+++ Proj-Zero/src/main/java/com/kim/repositories/ItemArray.java
@@ -1,5 +1,37 @@
 package com.kim.repositories;
 
-public class ItemArray {
+import com.kim.models.Item;
+
+public class ItemArray implements ItemDAOAble {
+
+	@Override
+	public Item[] getAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item getItemById(int Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addItem(Item item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String editItemById(int itemID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteItemById(int itemID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
